ce7c4864a26bfd49f713e1a9d2b2654d7cad4003
Blake Azuela <blake.azuela@bissell.com>
2026-03-03 07:49:34 -0500

    chore(doc) - added modification comment to frozen.c as required per Apache License Agreement (Section 4b)

--- frozen.c
+++ frozen.c
@@ -14,6 +14,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Modified by BISSELL Homecare, Inc. - see git history for changes.
  */
 
 #define _CRT_SECURE_NO_WARNINGS /* Disable deprecation warning in VS2005+ */
